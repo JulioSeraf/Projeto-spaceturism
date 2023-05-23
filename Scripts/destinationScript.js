735a0262c540c40187bcc2f7472e8ce17f7f137b
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-05-23 21:29:10 +0200

    testando script

---
+++ Scripts/destinationScript.js
@@ -0,0 +1,7 @@
+
+
+const bts = document.querySelectorAll('.bts');
+for(var i = 0; i < bts.length; i++){
+    bts[i].addEventListener('click' , botoes)
+  }
+ 
--- Scripts/homeScript.js
+++ Scripts/homeScript.js
@@ -1,35 +1,26 @@
-// // menu-------------------------------------------------
-// const openn = document.getElementById('open');
-// const cierre = document.getElementById('cierre');
-// const menu = document.getElementsByTagName('nav')[0];
-// const explore = document.querySelector('.explore');
+// menu-------------------------------------------------
+const menu = document.getElementsByTagName('nav')[0];
+const cierre = document.getElementById('cierre');
+const openn = document.getElementById('open');
+const explore = document.querySelector('.explore');
+cierre.addEventListener('click', ()=>{
+    cierre.style.display = 'none';
+    openn.style.display = 'block';
+    menu.style.top = '-320px';
+});
 
-// cierre.addEventListener('click', ()=>{
-//     cierre.style.display = 'none';
-//     openn.style.display = 'block';
-//     menu.style.top = '-320px';
-// });
+openn.addEventListener('click', ()=>{
+    cierre.style.display = 'block';
+    openn.style.display = 'none';
+    menu.style.top = '0%';
+});
 
-// openn.addEventListener('click', ()=>{
-//     cierre.style.display = 'block';
-//     openn.style.display = 'none';
-//     menu.style.top = '0%';
-// });
-
-// explore.addEventListener('click',()=>{
-//     if(menu.style.top == '0%'){
-//         menu.style.top = '-320px';
-//     }else{
-//         menu.style.top = '0%';
-//         menu.style.transition = '1s all';
-//     }
-// });
-
-let bts = document.querySelectorAll('.bts');
-
-for(var i = 0; i < bts.length; i++){
-    bts[i].addEventListener('click', ()=>{
-    })
-}
-
+explore.addEventListener('click',()=>{
+    if(menu.style.top == '0%'){
+        menu.style.top = '-320px';
+    }else{
+        menu.style.top = '0%';
+        menu.style.transition = '1s all';
+    }
+});
 
