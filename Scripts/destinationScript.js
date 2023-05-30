449578876180cfb4b8685f27b9a6f9194beace38
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-05-30 18:56:41 +0200

    testando Script de destination

--- Scripts/destinationScript.js
+++ Scripts/destinationScript.js
@@ -1,3 +1,4 @@
-function moonDisplay(){
-  document.getElementById('divmars').style.display ='block'
+const telas = document.querySelectorAll(".telas")
+for( var i in telas){
+console.log(telas[i].id)
 }
